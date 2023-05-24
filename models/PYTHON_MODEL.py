cd8c7b39596e761cb32ad64f6be039e6f365e94e
Nassim Lamara <nlamara@finaxys.com>
2023-05-24 14:58:51 +0200

    add VOLUME column

--- models/PYTHON_MODEL.py
+++ models/PYTHON_MODEL.py
@@ -5,8 +5,11 @@ import snowflake.snowpark as snowpark
 def model(dbt, session): 
     
     ESG_df = session.table('TRADE_DB.PUBLIC.BRONZE_TABLE_ESG')
+    
     PRICE_df = session.table('TRADE_DB.PUBLIC.BRONZE_TABLE_PRICE')
-    dataframe = ESG_df.join(PRICE_df, "ISIN")
+   
+    #join the two dfs 'ESG_df' and 'PRICE_df'
+    dataframe = ESG_df.join(PRICE_df, ESG_df['ISIN']==PRICE_df['OBNAME'])
 
 
     return dataframe

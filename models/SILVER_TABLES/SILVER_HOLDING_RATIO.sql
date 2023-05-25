5faff94e97d32d7122541a97a04e6259b12bb2bb
Fabien CHERER <fcherer@finaxys.com>
2023-05-25 10:58:01 +0200

    [DATADAY] New tables

---
+++ models/SILVER_TABLES/SILVER_HOLDING_RATIO.sql
@@ -0,0 +1,22 @@
+WITH OB_HOLDING AS (
+    SELECT
+      a.NAME as agentname, 
+      LEFT(a.OBNAME, 12) as orderbook, 
+      a.INVESTS * a.LASTFIXESPRICE as ob_holding
+    FROM {{ref('BRONZE_TABLE_AGENT')}} a
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY agentname, orderbook ORDER BY  a.TIMESTAMP desc) = 1)
+, HOLDING AS (
+    SELECT AGENTNAME, 
+        sum(ob_holding) as holding
+    FROM OB_HOLDING GROUP BY AGENTNAME
+), HOLDING_RATIO AS (
+    SELECT 
+        obh.AGENTNAME,
+        obh.orderbook, 
+        DIV0(obh.ob_holding,h.holding) as holding_ratio, 
+        h.holding as holding, 
+        obh.ob_holding 
+    FROM OB_HOLDING obh, HOLDING H
+    WHERE obh.agentname = h.agentname
+)
+SELECT * FROM HOLDING_RATIO ORDER BY holding desc
--- models/SILVER_TABLES/SILVER_PRICE_ANALYTICS.py
+++ models/SILVER_TABLES/SILVER_PRICE_ANALYTICS.py
@@ -2,22 +2,25 @@
 
 def model(dbt, session): 
 
-    CLASSIFICATION_df = dbt.ref("BRONZE_TABLE_CLASSIFICATION")
+    classification_df = dbt.ref("BRONZE_TABLE_CLASSIFICATION")
     
-    PRICE_df = dbt.ref("BRONZE_TABLE_PRICE")
+    price_df = dbt.ref("BRONZE_TABLE_PRICE")
+
+    #Transform to Pandas
+    price_df = price_df.to_pandas()
 
     #Lambda function to cumpute volume
-    f = lambda x : x['PRICE'] * x['QUANTITY']
-    PRICE_df = PRICE_df.to_pandas()
-    
+    #TODO
+    f = lambda x : x
+
     #use apply fucntion to parse the df
-    PRICE_df['VOLUME'] = PRICE_df.apply(f, axis=1)
+    price_df['VOLUME'] = price_df.apply(f, axis=1)
     #reorder the columns in the df
-    vol = PRICE_df.pop('VOLUME')
-    PRICE_df.insert(4, 'VOLUME', vol)
+    vol = price_df.pop('VOLUME')
+    price_df.insert(4, 'VOLUME', vol)
     #convert the pandas df to snowpark df
-    PRICE_df = session.create_dataframe(PRICE_df)
-    #join the two dfs 'ESG_df' and 'PRICE_df'
-    dataframe = CLASSIFICATION_df.join(PRICE_df, CLASSIFICATION_df['ISIN']==PRICE_df['OBNAME'])
+    price_df = session.create_dataframe(price_df)
+    #join the two dfs 'ESG_df' and 'price_df'
+    dataframe = classification_df.join(price_df, classification_df['ISIN']==price_df['OBNAME'])
 
     return dataframe

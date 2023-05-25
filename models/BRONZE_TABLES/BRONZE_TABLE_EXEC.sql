9635c45a705192986f1b617cc6e6e961746b147a
Fabien CHERER <fcherer@finaxys.com>
2023-05-25 10:58:01 +0200

    [DATADAY] New tables

--- models/BRONZE_TABLES/BRONZE_TABLE_EXEC.sql
+++ models/BRONZE_TABLES/BRONZE_TABLE_EXEC.sql
@@ -1,14 +1,8 @@
+/*
 SELECT
-$1:timestamp::varchar::TIMESTAMP TIMESTAMP,
-$1:obName::STRING OBNAME,
-$1:orderType::STRING ORDERTYPE,
-$1:sender::STRING SENDER,
-$1:extId::STRING EXTID,
-$1:quantity::NUMBER QUANTITY,
-$1:direction::STRING DIRECTION,
-$1:price::NUMBER PRICE,
-$1:validity::NUMBER VALIDITY
+1
 FROM
 TRADE_DB.PUBLIC.TRADE_BRONZE
 WHERE
 $1:type = 'Exec'
+ */
